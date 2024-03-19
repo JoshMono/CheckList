5b6e3db64fea182b8cef229f7ea2df20b32cc26a
Josh <68841480+JoshMono@users.noreply.github.com>
2024-03-19 12:54:18 +1100

    Final Comit

--- CheckList/CheckList.py
+++ CheckList/CheckList.py
@@ -14,25 +14,39 @@ import sv_ttk
 # Fuctions
 
 
-def ClearCancel():
-    global comfirmClear
+def Save():
+    listOfAllEntries = CheckList.get_children()
+    file = open('dataBase.txt', 'w')
+    file.write("")
+    file.close()
+
+    file = open('dataBase.txt', 'a')
+    
+    for each in listOfAllEntries:
+        item = CheckList.item(each)
+        file.write(f"{item.get('values')[0]};")
+    
+    file.close()
+
+
+
+def ClearCancel(comfirmClear):
     comfirmClear = False
     
-    EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue())
-    AddBTN.config(bg='spring green', command=lambda: AddNewTask())
-    DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue())
+    EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+    AddBTN.config(bg='spring green', command=lambda: AddNewTask(inAddMode, EntryField))
+    DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue(comfirmDelete))
     
-    ClearBTN.config(text='Clear', bg='turquoise2', command=lambda: Clear())
-    SaveBTN.config(text='Save', bg='slateblue2', command=lambda: None)
+    ClearBTN.config(text='Clear', bg='turquoise2', command=lambda: Clear(comfirmClear))
+    SaveBTN.config(text='Save', bg='slateblue2', command=lambda: Save())
 
 
 comfirmClear = False
-def Clear():
-    global comfirmClear
+def Clear(comfirmClear):
     if comfirmClear == False:
         comfirmClear = True
-        ClearBTN.config(text="Cancel", bg="red2", command=lambda: ClearCancel())
-        SaveBTN.config(text="Ok", bg="spring green", command=lambda: Clear())
+        ClearBTN.config(text="Cancel", bg="red2", command=lambda: ClearCancel(comfirmClear))
+        SaveBTN.config(text="Ok", bg="spring green", command=lambda: Clear(comfirmClear))
         
         AddBTN.config(bg='gray62', command=lambda: None)
         EditBTN.config(bg='gray62', command=lambda: None)
@@ -42,37 +56,34 @@ def Clear():
         for i in CheckList.get_children():
             CheckList.delete(i)
             
-        ClearCancel()
+        ClearCancel(comfirmClear)
         
 
 
-def CancelAdd():
-    global EntryField
-    global inAddMode
-    
+def CancelAdd(EntryField, inAddMode):
     inAddMode = False
     EntryField.destroy()
     
-    EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue())
-    AddBTN.config(bg='spring green', command=lambda: AddNewTask())
-    DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue())
+    EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+    AddBTN.config(bg='spring green', command=lambda: AddNewTask(inAddMode, EntryField))
+    DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue(comfirmDelete))
 
 
 inAddMode = False
 EntryField = None
 
-def AddNewTask():
-    global inAddMode
-    global EntryField
-    
+def AddNewTask(inAddMode, EntryField):   
     if inAddMode == False:
         EntryField = Text(menuFrame)
         EntryField.place(relx=0.5,rely=0.25,relwidth=0.95, relheight=0.1, anchor='center')
         
         AddBTN.config(bg='gray62', command=lambda: None)
-        DeleteBTN.config(text='Cancel',bg='red2', command=lambda: CancelAdd())
-        EditBTN.config(text='Save', bg='spring green', command=lambda: AddNewTask())
+        DeleteBTN.config(text='Cancel',bg='red2', command=lambda: CancelAdd(EntryField, inAddMode))
+        EditBTN.config(text='Save', bg='spring green', command=lambda: AddNewTask(inAddMode, EntryField))
         
+        SaveBTN.config(bg='gray62', command=lambda: None)
+        ClearBTN.config(bg='gray62', command=lambda: None)
+
         inAddMode = True
 
     else:
@@ -85,24 +96,25 @@ def AddNewTask():
             CheckList.insert("", 'end', values=text.replace(" ", "\ "))
             EntryField.destroy()
             
-            EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue())
-            AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask())
+            EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+            AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask(inAddMode, EntryField))
             AddBTN.place(relx=0.5,rely=0.4,relwidth=0.6, relheight=0.08, anchor='center')
-            DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue())
+            DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue(comfirmDelete))
+            ClearBTN.config(text='Clear', bg='turquoise2', command=lambda: Clear(comfirmClear))
+            SaveBTN.config(text='Save', bg='slateblue2', command=lambda: Save())
 
 
-def CancelEdit():
-    global inEditMode
-    global EntryField
-    global tempFocus
+def CancelEdit(inEditMode, EntryField, tempFocus):
     inEditMode = False
     EntryField.destroy()
     CheckList.selection_remove(tempFocus)
     tempFocus = None
     
-    EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue())
-    AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask())
-    DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue())
+    EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+    AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask(inAddMode, EntryField))
+    DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue(comfirmDelete))
+    ClearBTN.config(text='Clear', bg='turquoise2', command=lambda: Clear(comfirmClear))
+    SaveBTN.config(text='Save', bg='slateblue2', command=lambda: Save())
     
 
 
@@ -111,30 +123,31 @@ inEditMode = False
 tempFocus = None
 
 
-def EditValue():
-    global inEditMode
-    global tempFocus
-    global EntryField
+def EditValue(inEditMode, tempFocus, EntryField):
+    print(inEditMode)
     if inEditMode == False:
+        
         try:
-            
             tempFocus = CheckList.selection()[0]
             print()
             if tempFocus != '':
                 
-                DeleteBTN.config(text='Cancel',bg='red2', command=lambda: CancelEdit())
-                EditBTN.config(text='Save', bg='spring green')
-                
+                DeleteBTN.config(text='Cancel',bg='red2', command=lambda: CancelEdit(inEditMode, EntryField, tempFocus))
+                SaveBTN.config(bg='gray62', command=lambda: None)
+                ClearBTN.config(bg='gray62', command=lambda: None)                
+
                 item = CheckList.item(tempFocus)
                 text = item.get('values')[0]
                 print(item.get('values'))
-                AddBTN.config(text=f"{text}", bg='white', command=lambda: None)
+                AddBTN.config(text=f"{text}", bg='white', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
                 AddBTN.place(relx=0.5,rely=0.4,relwidth=0.95, relheight=0.08, anchor='center')
                 EntryField = Text(menuFrame)
                 EntryField.place(relx=0.5,rely=0.25,relwidth=0.95, relheight=0.1, anchor='center')
                 EntryField.delete(1.0, END)
                 EntryField.insert(1.0 ,text)
                 inEditMode = True
+                EditBTN.config(text='Save', bg='spring green', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+                print()
         except:
             pass
             
@@ -146,45 +159,51 @@ def EditValue():
         EntryField.destroy()
         CheckList.selection_remove(tempFocus)
         tempFocus = None
-        EditBTN.config(text='Edit', bg='chocolate1')
-        AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask())
+        EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+        AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask(inAddMode, EntryField))
         AddBTN.place(relx=0.5,rely=0.4,relwidth=0.6, relheight=0.08, anchor='center')
-        DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue())
+        DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue(comfirmDelete))
+        ClearBTN.config(text='Clear', bg='turquoise2', command=lambda: Clear(comfirmClear))
+        SaveBTN.config(text='Save', bg='slateblue2', command=lambda: Save())
         
 
-def ConfirmDelete(comfirmed, tempRecord):
+def ConfirmDelete(comfirmed, tempRecord, comfirmDelete):
     if comfirmed:
-        global comfirmDelete
         comfirmDelete = True
-        DeleteBTN.config(command=lambda: DeleteValue())
+        DeleteBTN.config(command=lambda: DeleteValue(comfirmDelete))
         tempRecord = CheckList.selection()[0]   
         CheckList.delete(tempRecord)
-        AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask())
+        AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask(inAddMode, EntryField))
         AddBTN.place(relx=0.5,rely=0.4,relwidth=0.6, relheight=0.08, anchor='center')
-        DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue())
-        EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue())
+        DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue(comfirmDelete))
+        EditBTN.config(text='Edit', bg='chocolate1', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+        ClearBTN.config(text='Clear', bg='turquoise2', command=lambda: Clear(comfirmClear))
+        SaveBTN.config(text='Save', bg='slateblue2', command=lambda: Save())
         comfirmDelete = False
     else:
         CheckList.selection_remove(tempRecord)
-        AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask())
+        AddBTN.config(text='Add', bg='spring green', command=lambda: AddNewTask(inAddMode, EntryField))
         AddBTN.place(relx=0.5,rely=0.4,relwidth=0.6, relheight=0.08, anchor='center')
-        DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue())
-        EditBTN.config(text='Edit',bg='chocolate1', command=lambda: EditValue())
+        DeleteBTN.config(text='Delete', bg='red2', command=lambda: DeleteValue(comfirmDelete))
+        EditBTN.config(text='Edit',bg='chocolate1', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+        ClearBTN.config(text='Clear', bg='turquoise2', command=lambda: Clear(comfirmClear))
+        SaveBTN.config(text='Save', bg='slateblue2', command=lambda: Save())
         comfirmDelete = False
 
 
 comfirmDelete = False
-def DeleteValue():
-    global comfirmDelete
+def DeleteValue(comfirmDelete):
     try:
         tempRecord = CheckList.selection()[0]
         item = CheckList.item(tempRecord)
         text = item.get('values')[0]
         if comfirmDelete == False:
             AddBTN.config(text=f"{text}", bg='white', command=lambda: None)
+            SaveBTN.config(bg='gray62', command=lambda: None)
+            ClearBTN.config(bg='gray62', command=lambda: None)
             AddBTN.place(relx=0.5,rely=0.4,relwidth=0.95, relheight=0.08, anchor='center')
-            EditBTN.config(text='Ok', bg='spring green', command=lambda: ConfirmDelete(True, tempRecord))
-            DeleteBTN.config(text='Cancel', bg='red2', command=lambda: ConfirmDelete(False, tempRecord))
+            EditBTN.config(text='Ok', bg='spring green', command=lambda: ConfirmDelete(True, tempRecord, comfirmDelete))
+            DeleteBTN.config(text='Cancel', bg='red2', command=lambda: ConfirmDelete(False, tempRecord, comfirmDelete))
             
     except:
         pass      
@@ -200,6 +219,8 @@ style = Style()
 
 
 
+            
+
 # Frames 
 menuFrame = Frame(root, bg='gray88')
 mainFrame = Frame(root)
@@ -210,12 +231,12 @@ mainFrame.place(relx=0.2,rely=0, relwidth=.8, relheight=1)
 
 # Menu Frame
 Title = Label(menuFrame, text='Check List', bg='gray88')    
-AddBTN = Button(menuFrame, text="Add", bg='spring green',  command=lambda: AddNewTask())
-EditBTN = Button(menuFrame, text="Edit", bg='chocolate1', command=lambda: EditValue())
-DeleteBTN = Button(menuFrame, text="Delete", bg='red2', command=lambda: DeleteValue())
+AddBTN = Button(menuFrame, text="Add", bg='spring green',  command=lambda: AddNewTask(inAddMode, EntryField))
+EditBTN = Button(menuFrame, text="Edit", bg='chocolate1', command=lambda: EditValue(inEditMode, tempFocus, EntryField))
+DeleteBTN = Button(menuFrame, text="Delete", bg='red2', command=lambda: DeleteValue(comfirmDelete))
 
-SaveBTN = Button(menuFrame, text="Save", bg='slateblue2', command=lambda: None)
-ClearBTN = Button(menuFrame, text="Clear", bg='turquoise2', command=lambda: Clear())
+SaveBTN = Button(menuFrame, text="Save", bg='slateblue2', command=lambda: Save())
+ClearBTN = Button(menuFrame, text="Clear", bg='turquoise2', command=lambda: Clear(comfirmClear))
 
 
 Title.place(relx=0,rely=0,relwidth=1, relheight=0.1)
@@ -236,5 +257,21 @@ CheckList['columns'] = ("1")
 CheckList['show'] = 'headings'
 CheckList.column('1', anchor='w')
 CheckList.heading('1', text='Tasks')
+
+
+# Open File
+
+file = open('dataBase.txt', 'r')
+name = ''
+
+for _ in file.readline():
+    
+    if _ == ';':
+        CheckList.insert("", 'end', values=name.replace(" ", "\ "))
+        name = ""
+    else:
+        name = name + _
+
+file.close()
 
 root.mainloop()
